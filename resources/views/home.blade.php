e56113e76e43851aec99c236d2f3b3cf51e71bd5
Alessio-Tosoni <alessiotosoni988@gmail.com>
2024-01-24 21:35:39 +0100

    crea model e pagecontroller(non funzionante)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Dati da config/data.js: {{ $home }}</h1>
+    <h1>Movies</h1>
+    
+    <ul>
+        @foreach ($libri as $libro)
+            <li>
+                <span>{{ $libro->name }}</span>
+                <img class="copertina" src="{{ $libro->cover }}" alt="">
+            </li>
+        @endforeach
+    </ul>
+   
+
     <p>This is my paragraph content.</p>
 @endsection
